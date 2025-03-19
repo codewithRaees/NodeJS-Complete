a2cb5fa4913c90803ec8975337c23ed6da73bbd2
Raees <118157840+codewithRaees@users.noreply.github.com>
2025-03-19 12:08:00 +0500

    Create app.js

---
+++ Building Restful CRUD APIs With Express.js/190_Intro to Express.js/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express()
+app.disable("x-powered-by")
+app.get("/", (req, res) => {
+    res.send("Hello World")
+
+    // res.setHeader("Content-Type", "text/html")
+    // res.end("Hello World")
+})
+
+
+app.listen(
+    3000,
+    () => console.log("Server running on port 3000")
+)
