3302e34cee93024c3bbd9a7dea80ad7357755f7a
Your Name <hafizraiskundi@gmail.com>
2025-04-12 12:34:13 +0500

    Lecture 11 and 12 of Section 11

---
+++ 11_Building RESTful CRUD APIs with Express.js/11_dynamic-routing-in-express/11_dynamic-routing-in-express-server/app.js
@@ -0,0 +1,69 @@
+import express from "express";
+import { readdir, rename, rm } from "fs/promises";
+
+const app = express();
+
+app.use(express.json());
+
+//Enabling CORS
+app.use((req, res, next) => {
+  res.set('Access-Control-Allow-Origin', "*")
+  res.set('Access-Control-Allow-Methods', '*')
+  res.set('Access-Control-Allow-Headers', '*')
+  next()
+})
+
+//READ
+app.get('/', async (req, res) => {
+ const filesList = await readdir('./storage')
+ res.json(filesList)
+})
+app.get('/:filename', (req, res) => {
+  const { filename } = req.params 
+  if (req.query.action === 'download') {
+    res.set('Content-Disposition', 'attachment')
+  }
+  res.sendFile(`${import.meta.dirname}/storage/${filename}`)
+})
+
+
+// Delete
+app.delete('/:filename', async (req, res) => {
+  const { filename } = req.params
+  const filePath = `./storage/${filename}`
+  console.log(filePath)
+  try {
+    rm(filePath)
+    res.json({ message: 'File Deleted Successfully' })
+  } catch (err) {
+   res.status(404).json({ message: 'File Not Found!' }) 
+  }
+  
+})
+
+// RENAME
+app.patch('/:filename', async (req, res) => {
+  const { filename } = req.params
+  const newFilename = req.body.newFilename
+  const filePath = `./storage/${filename}`
+  const newFilePath = `./storage/${newFilename}`
+  try {
+    await rename(filePath, newFilePath)
+    res.json({ message: 'Renamed Successfully' })
+  } catch (err) {
+    res.status(404).json({ message: 'File Not Found!' })
+  }
+})
+
+// // Update
+// app.patch("/:filename", async (req, res) => {
+//   const { filename } = req.params;
+//   await rename(`./storage/${filename}`, `./storage/${req.body.newFilename}`);
+//   res.json({ message: "Renamed" });
+// });
+
+
+
+app.listen(4000, () => {
+  console.log(`Server Started`);
+});
